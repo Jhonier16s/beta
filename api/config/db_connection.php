68625df789401190f3cf7c4191a0de103e3e28f0
Jhonier <jhonier.becerra@zoomcem.com>
2024-09-25 22:07:57 -0500

    Add - connection database

---
+++ api/config/db_connection.php
@@ -0,0 +1,36 @@
+<?php
+/*$numero1 = 20;
+$numero2 = 30;
+$suma = $numero1 + $numero2;
+echo $suma;*/
+
+
+/* Postgres Database connection
+Developer : Jhonier
+*/
+
+$host      = "localhost"; //127.0.0.1
+$username  = "postgres";
+$password  = "1234"; //unicesmag
+$dbname    = "beta";
+$port      = "5432";
+
+$data_connection = "
+host     = $host
+port     = $port
+dbname   = $dbname
+user = $username
+password = $password
+";
+
+$conn = pg_connect($data_connection);
+
+if (!$conn){
+    die("Connection failed: ".pg_last_error());
+}else{
+    echo "Connected successfully";
+}
+
+pg_close($conn);
+
+?> 
